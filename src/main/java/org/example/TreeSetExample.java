77ab9955dd8adb5981621be800b30d08ba9f55c5
Tringapps Subhiksha <123531014+subhi-29@users.noreply.github.com>
2023-02-13 12:59:49 +0530

    Update TreeSetExample.java

--- src/main/java/org/example/TreeSetExample.java
+++ src/main/java/org/example/TreeSetExample.java
@@ -13,21 +13,21 @@ public class TreeSetExample {
         med.add(724);
         med.add(672);
         med.add(900);
-        l.info("Print the medical price using Treeset it will print in sorted order: "+med);
+         l.log(Level.INFO, () ->"Print the medical price using Treeset it will print in sorted order: "+med);
         TreeSet med1=new TreeSet();
         l.info("Add new price to the existing treeset");
         int n=sc.nextInt();
         med1.add(n);
         med1.addAll(med);
-        l.info("Print the new price with the existing price "+med1);
+          l.log(Level.INFO, () ->"Print the new price with the existing price "+med1);
        int f= (int) med1.first();
-       l.info("Print the First price of the Treeset "+f);
+        l.log(Level.INFO, () ->"Print the First price of the Treeset "+f);
        int la  = (int) med1.last();
-        l.info("Print the Last price of the Treeset "+la);
-        l.info("It returns and remove the element using pollfirst method: "+med1.pollFirst());
-        l.info("It returns and remove the element using polllast method: "+med1.pollLast());
-        l.info("" +med1);
-        l.info("It returns the price using headset method "+med1.headSet(672,true));
-        l.info("It returns the price using tailset method "+med1.tailSet(500,true));
+          l.log(Level.INFO, () ->"Print the Last price of the Treeset "+la);
+          l.log(Level.INFO, () ->"It returns and remove the element using pollfirst method: "+med1.pollFirst());
+          l.log(Level.INFO, () ->"It returns and remove the element using polllast method: "+med1.pollLast());
+         l.log(Level.INFO, () ->"" +med1);
+         l.log(Level.INFO, () ->"It returns the price using headset method "+med1.headSet(672,true));
+         l.log(Level.INFO, () ->"It returns the price using tailset method "+med1.tailSet(500,true));
     }
 }
