3b351df566e6ffd3738240326b6685937fc2e98b
Tringapps Subhiksha <123531014+subhi-29@users.noreply.github.com>
2023-02-13 12:10:18 +0530

    Update Hashmap.java

--- src/main/java/org/example/Hashmap.java
+++ src/main/java/org/example/Hashmap.java
@@ -27,33 +27,33 @@ import java.util.logging.Logger;
             med.put(id[1],name[1]);
             med.put(id[2],name[2]);
             l.info("Print the elements using Hashmap");
-            System.out.println("Iterating Hashmap...");
+            l.info("Iterating Hashmap...");
             for(Map.Entry m : med.entrySet()) {
-                System.out.println(m.getKey()+" "+m.getValue());
+                  l.log(Level.INFO, () ->(m.getKey()+" "+m.getValue()));
             }
             l.info("Print the initial elements of hashmap" +med);
             med.putIfAbsent(103, "Acetaminophen");
             System.out.println("After putIfAbsent() method print the elements using hashmap");
             for(Map.Entry m:med.entrySet()){
-                System.out.println(m.getKey()+" "+m.getValue());
+                  l.log(Level.INFO, () ->(m.getKey()+" "+m.getValue()));
             }
             HashMap<Integer,String> med1=new HashMap<Integer,String>();
             med1.put(104,"Ravi");
             System.out.println("Updating the elements using Hashmap ");
             for(Map.Entry m:med1.entrySet()){
-                System.out.println(m.getKey()+" "+m.getValue());
+                l.log(Level.INFO, () ->(m.getKey()+" "+m.getValue()));
             }
             System.out.println("Give the remove elements using hashmap");
             int n=sc.nextInt();
             med.remove(n);
-            System.out.println("After removing the list of elements using hashmap key "+med);
-            System.out.println("Enter the name to remove");
+            l.info("After removing the list of elements using hashmap key "+med);
+            l.info("Enter the name to remove");
             String name1=sc1.nextLine();
             med.remove(n, name1);
-            System.out.println("After removing the list of elements using hashmap key value pair: "+med);
-            System.out.println("To clear the elements using Hashmap");
+            l.info("After removing the list of elements using hashmap key value pair: "+med);
+            l.info("To clear the elements using Hashmap");
             med.clear();
-            System.out.println("To check hashmap is empty or not?");
-            System.out.println(med.isEmpty());
+            l.info("To check hashmap is empty or not?");
+            l.info(med.isEmpty());
         }
     }
